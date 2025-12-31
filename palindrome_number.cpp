d3a6b7a94b918bf9948c45267541032127ec7aab
Stranger4uu <yashsaini4824@gmail.com>
2025-12-31 20:56:05 +0530

    updated 'Palindrome number' file

--- palindrome_number.cpp
+++ palindrome_number.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-
-
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
+
     int reverse_number = 0;
     int dup = n;
-    while(n>0){
+
+    while (n > 0) {
         int last_number = n % 10;
-        reverse_number = (reverse_number * 10) + last_number;
+        reverse_number = reverse_number * 10 + last_number;
         n = n / 10;
     }
-    if (dup==reverse_number) cout<<"true";
-    else cout<<"false";
-}
 
+    if (dup == reverse_number)
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;   
+}
