62fb5e368537a66fc1cd19f4b7c5a3b7d453818e
Stranger4uu <yashsaini4824@gmail.com>
2026-02-02 20:26:26 +0530

    did some code of class to understand the logic of class

---
+++ class.cpp
@@ -0,0 +1,58 @@
+
+// class first
+// {
+//     public:
+//         int a,b;
+//         void name();
+// };
+// void first:: name() {
+//     cout << "Yash" << "\n";
+// }
+
+// int main() {
+//     first f1,f2;
+//     f1.a = 10;
+//     f2.b = 30;
+//     cout<<f1.a<<"\n";
+//     cout<<f2.b<<"\n";
+//     f1.name();
+// }
+
+// create a class named item , data in public is name(string) and price (int) then create 2 function 
+// 1st function named set to set value of name and price (outside the class)
+// 2nd function named show to show the name and price ( inside the class)
+// objects 3 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class item{
+    public:
+        string name;
+        int price;
+        void show(){
+            cout<<name;
+            cout<<endl; 
+            cout<<price<<"$";
+            cout<<endl; 
+        }
+        void set();
+};
+
+void item:: set(){
+    cout<<"Enter the product name : ";
+    cin>>name;
+    cout<<"Enter the product price : ";
+    cin>>price;
+    
+}
+
+int main(){
+    item obj; 
+    obj.set();
+    obj.show(); 
+    return 0;
+}
